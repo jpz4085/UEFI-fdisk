96d4486b7f35d8e0ff687eb96d61b645b75f6180
jpz4085 <jpz408@yahoo.com>
2024-09-28 17:46:25 -0400

    Add support for command line arguments from UEFI Shell. Check that shell path is valid and EFI handle is a legitimate block device. Get program name from first argument and shift to lower case.

--- efi_main.c
+++ efi_main.c
@@ -6,8 +6,8 @@
  * UEFI fdisk est un portage de util-linux fdisk vers UEFI/BIOS.
  * Ce fichier a été initié par Bernard Burette en janvier 2014.
  *
- * All this work is copyleft Bernard Burette.
- * Gauche d'auteur Bernard Burette.
+ * Original work is copyleft Bernard Burette.
+ * Modifications are copyleft Joseph Zeller.
  *
  * This program is distributed under the terms of the GNU General Public
  *  License version 2 or more.
@@ -31,10 +31,12 @@
 #include "uefi.h"
 #include "debug.h"
 #include <unistd.h>
+#include <efishell.h>
+#include <efiprot.h>
+#include "libmy/converter.h"
 
 #include "../config.h"
 #include "../include/strutils.h"
-
 
 /**
  * La table de tous les périphériques disque.
@@ -49,9 +51,11 @@ UINTN HandleCount ;
 
 
 /**
- * La table des disques utilisables.
+ * table: Table of fixed disks when no argument is present.
+ * Device: Handle of the block device passed by UEFI Shell.
  */
 static EFI_HANDLE * table ;
+static EFI_HANDLE * Device;
 
 
 /**
@@ -191,20 +195,162 @@ static EFI_STATUS FindPartitionableDevices( void )
     return EFI_SUCCESS;
 }
 
+/**
+ * Read in arguments from the UEFI Shell command line.
+ */
+static EFI_STATUS
+get_args(EFI_HANDLE image, UINTN *argc, CHAR16 ***argv)
+{
+    EFI_STATUS status;
+    EFI_SHELL_PARAMETERS_PROTOCOL *ShellParameters;
+    EFI_GUID ShellParametersProtocolGuid = EFI_SHELL_PARAMETERS_PROTOCOL_GUID;
+
+    status = uefi_call_wrapper(BS->OpenProtocol, 6,
+                               image, &ShellParametersProtocolGuid,
+                               (VOID **)&ShellParameters, image, NULL,
+                               EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    if (EFI_ERROR(status)) {
+    	return status;
+    }
+
+    *argc = ShellParameters->Argc;
+    *argv = ShellParameters->Argv;
+
+    status = uefi_call_wrapper(BS->CloseProtocol, 4, image,
+                               &ShellParametersProtocolGuid, image, NULL);
+    return status; 
+}
+
+
+/**
+ * Get the handle of the block device passed through the command line.
+ * Display an error message and return if unable to get device path or
+ * handle or device seems to be a partition or DVD rather than a disk.
+ */
+static EFI_STATUS
+get_device_handle(CONST CHAR16 *pname, CONST CHAR16 *Mapping)
+{
+	EFI_STATUS status;
+	EFI_BLOCK_IO *BlkIo;
+	EFI_SHELL_PROTOCOL *Shell; 
+	CONST EFI_DEVICE_PATH_PROTOCOL *EfiDevPath;
+	EFI_GUID ShellProtocolGuid = EFI_SHELL_PROTOCOL_GUID;
+	EFI_GUID BlockIoProtocolGuid = EFI_BLOCK_IO_PROTOCOL_GUID;
+        
+        status = uefi_call_wrapper(BS->LocateProtocol, 3,
+        			  &ShellProtocolGuid,
+        			  NULL, (VOID **)&Shell);
+        
+        if (EFI_ERROR(status)) {
+        	Print(L"Unable to access shell protocol data. \n");
+        	Print(L"Run this program without arguments. \n");
+        	return status;
+        }
+        
+        EfiDevPath = (VOID *)uefi_call_wrapper(Shell->GetDevicePathFromMap, 1,
+        			       Mapping);
+        
+        if (!EfiDevPath) {
+        	Print(L"%s: Unable to get device path for %s. \n", pname, Mapping);
+        	status = EFI_ABORTED;
+        	return status;
+        }
+        
+        status = uefi_call_wrapper(BS->LocateDevicePath, 3,
+        			  &BlockIoProtocolGuid,
+        			  &EfiDevPath, &Device);
+        
+        if (EFI_ERROR(status)) {
+        	Print(L"%s: Unable to get device handle for %s. \n", pname, Mapping);
+        	return status;
+        }
+        
+        status = uefi_call_wrapper(BS->HandleProtocol, 3,
+        			  Device, &BlockIoProtocolGuid,
+        			  &BlkIo);
+        
+        if (EFI_ERROR(status)) {
+        	Print(L"%s: Unable to get device data for %s. \n", pname, Mapping);
+        	return status;
+        }
+        
+        if (BlkIo->Media->LogicalPartition) {
+        	Print(L"%s: Device %s appears to be a partition. \n", pname, Mapping);
+        	status = EFI_ABORTED;
+        	return status;
+        }
+        
+        if (!BlkIo->Media->MediaPresent) {
+        	Print(L"%s: Device %s does not have media present. \n", pname, Mapping);
+        	status = EFI_ABORTED;
+        	return status;
+        }
+        
+        if (BlkIo->Media->ReadOnly) {
+        	Print(L"%s: Device %s can not be partitioned: Read Only. \n", pname, Mapping);
+        	status = EFI_ABORTED;
+        	return status;
+        }
+
+	return status;
+}
+
+/**
+ * Shift all characters to lower case then
+ * get program name from end of file path.
+ */
+static CHAR16 *progname (CHAR16 *opt)
+{
+	CHAR16 *ptr;
+	static BOOLEAN islower;
+	
+	if (!islower) {
+		for (UINTN i = 0; opt[i]; i++)
+			StrLwr(&opt[i]);
+		islower = TRUE;
+	}
+	
+	for (ptr = NULL;; opt++) {
+		if (*opt == '\\'){
+			opt++;
+			ptr = (CHAR16 *)opt;
+		}
+		if (*opt == '\0')
+			return (ptr);
+	}
+}
+
+/**
+ *  Format program name to UTF-8 then copy to proginvoshrtname.
+ */
+void set_shortname(CHAR16 *optname, char *pshort)
+{
+	size_t length = (size_t)StrLen(optname);
+	size_t buffsz = utf16_to_utf8(optname, length, NULL, 0);
+	utf8_t* buffer = malloc(buffsz + 1);
+	if (!buffer) {
+		Print(L"%s: Unable to allocate buffer for name argument. \n", optname);
+		FreePool(buffer);
+		exit(5);
+    	}
+    	ZeroMem(buffer, buffsz + 1);
+	utf16_to_utf8(optname, length, buffer, buffsz);
+	buffer[buffsz + 1] = '\0';
+	strcpy(pshort, (const char*)buffer);
+	FreePool(buffer);
+}
 
 /**
  * Le nom de ce programme.
  */
-static const char proginvoshrtname[] = "fdisk.efi" ;
-char * program_invocation_short_name = (char*) proginvoshrtname ;
-
+static char proginvoshrtname[25];
+char *program_invocation_short_name = (char*) proginvoshrtname;
 
 /**
  * Les arguments passés à la fonction main().
  */
-static const char* argv[] = { proginvoshrtname , NULL , NULL } ;
+static const char* args[256];
 static const char* envp[] = { NULL } ;
-
 
 /**
  * La définition de la fonction main().
@@ -218,9 +364,10 @@ extern int main( int , const char ** , const char ** ) ;
  * @param SystemTable La table des HANDLE et fonction UEFI.
  * @return Un EFI_STATUS de fin d'exécution.
  *
- * Cette fonction est le point d'entrée dans le programme (appellée depuis
- * le module start-$(ARCH)). Elle fait des trucs EFI pour récupérer la
- * liste des disques puis appelle la fonction main() du fdisk classique.
+ * The efi_main function is the entry point for this program. It will
+ * present a list of fixed disks by searching the BlockIoProtocol when
+ * started without arguments or it will find the device handle of the
+ * block device passed through the UEFI Shell command line.
  */
 EFI_STATUS
 efi_main( EFI_HANDLE ImageHandle , EFI_SYSTEM_TABLE * SystemTable ) ;
@@ -230,67 +377,94 @@ efi_main( EFI_HANDLE ImageHandle , EFI_SYSTEM_TABLE * SystemTable )
 {
 	EFI_STATUS status ;
 	EFI_HANDLE Disk ;
+	UINTN argc = 0;
+	CHAR16 **argv;
+	
 	/* initialise la librairie GNU */
 	InitializeLib( ImageHandle , SystemTable ) ;
-#if 0
-	/* les information sur moi-même */
-	EFI_LOADED_IMAGE * loaded_image = NULL ;
-	EFI_GUID my_LoadedImageProtocol = LOADED_IMAGE_PROTOCOL ;
-	status = UEFI_call( SystemTable-> BootServices-> HandleProtocol ,
-		ImageHandle , & my_LoadedImageProtocol ,
-		(void **) & loaded_image ) ;
-	if ( EFI_ERROR( status ) ) {
-		__fortify_fail( "handleprotocol" ) ;
-	}
-	UEFI_dprintf( D_INFO , "Hello, world!\n" ) ;
-	UEFI_dprintf( D_INFO , "Image base            : %p\n" ,
-		loaded_image-> ImageBase ) ;
-	UEFI_dprintf( D_INFO , "Image size            : %ld\n" ,
-		loaded_image-> ImageSize ) ;
-	UEFI_dprintf( D_INFO , "efi_main              : %p\n" , efi_main ) ;
-	UEFI_dprintf( D_INFO , "Fin des initialisations, appel de main()\n" ) ;
-#endif
-	printf( "\nWelcome to UEFI fdisk (%s) version " VERSION "\n\n"
-		"List of hard disks found:\n" ,
-		program_invocation_short_name ) ;
-	FindPartitionableDevices() ;
-	putchar( '\n' ) ;
-
-	/* si un seul disque, le sélectionne, sinon... */
-	if ( max_table == 1 ) {
-		Disk = table[ 0 ] ;
-		printf( "Using Disk 1\n" ) ;
+	
+	/* Get arguments from UEFI Shell */
+	status = get_args(ImageHandle, &argc, &argv);
+	
+	/* Set the program name depending on the presence of auguments */
+	if (status == EFI_SUCCESS) {
+		set_shortname(progname(argv[0]), proginvoshrtname);
 	} else {
-		for(;;) {
-			char str[ 10 ] ;
-			int n ;
-			printf( "Disk number (1-%d): " , max_table ) ;
-			/* lit une ligne */
-			if ( fgets( str , sizeof( str ) , stdin ) == NULL ) {
-				/* fin de fichier ou erreur */
-				exit( 5 ) ;
-			}
-			/* si la fin de ligne n'a pas été lue */
-			if ( str[ strlen( str ) - 1 ] != '\n' ) {
-				while ( fgetc( stdin ) != '\n' ) {}
-			}
-			/* trouve le nombre saisi */
-			if ( sscanf( str , "%d" , & n ) != 1 ) {
-				/* erreur dans la ligne lue ? */
-				n = 0 ;
-			}
-			if ( n >= 1 && n <= max_table ) {
-				Disk = table[ n - 1 ] ;
-				break ;
+		strcpy(proginvoshrtname, "fdisk.efi");
+	}
+	
+	if (argc < 2) {
+		/* Display the original disk menu if less than two arguments */
+		printf( "\nWelcome to UEFI fdisk (%s) version " VERSION "\n\n"
+			"List of hard disks found:\n",
+			program_invocation_short_name ) ;
+		FindPartitionableDevices() ;
+		putchar( '\n' ) ;
+
+		/* si un seul disque, le sélectionne, sinon... */
+		if ( max_table == 1 ) {
+			Disk = table[ 0 ] ;
+			printf( "Using Disk 1\n" ) ;
+		} else {
+			for(;;) {
+				char str[ 10 ] ;
+				int n ;
+				printf( "Disk number (1-%d): " , max_table ) ;
+				/* lit une ligne */
+				if ( fgets( str , sizeof( str ) , stdin ) == NULL ) {
+					/* fin de fichier ou erreur */
+					exit( 5 ) ;
+				}
+				/* si la fin de ligne n'a pas été lue */
+				if ( str[ strlen( str ) - 1 ] != '\n' ) {
+					while ( fgetc( stdin ) != '\n' ) {}
+				}
+				/* trouve le nombre saisi */
+				if ( sscanf( str , "%d" , & n ) != 1 ) {
+					/* erreur dans la ligne lue ? */
+					n = 0 ;
+				}
+				if ( n >= 1 && n <= max_table ) {
+					Disk = table[ n - 1 ] ;
+					break ;
+				}
 			}
 		}
+		putchar( '\n' ) ;
+		argc = 2;
+		args[0] = proginvoshrtname;
+		args[1] = UEFI_disk_name(Disk);
+		hush_next_line() ;
+	} else {
+		/* Read in arguments from UEFI Shell and format to UTF-8 */
+		for (UINTN i = 0; i < argc; i++) {
+			size_t len = (size_t)StrLen(argv[i]);
+			size_t out_len = utf16_to_utf8(argv[i], len, NULL, 0);
+			utf8_t* out_buf = malloc(out_len + 1);
+			if (!out_buf) {
+				Print(L"%s: Unable to allocate buffer for arguments. \n", progname(argv[0]));
+				FreePool(out_buf);
+				exit(5);
+    			}
+    			ZeroMem(out_buf, out_len + 1);
+			utf16_to_utf8(argv[i], len, out_buf, out_len);
+			out_buf[out_len + 1] = '\0';
+			if (!CompareMem(out_buf, "-", 1) ||
+			    !CompareMem(out_buf, "--", 2) || i == 0) {
+			        args[i] = (const char*)out_buf;
+			} else {
+				status = get_device_handle(progname(argv[0]), argv[i]);
+				if (EFI_ERROR(status)) {
+					FreePool(out_buf);
+					exit(5);
+				}
+				args[i] = UEFI_disk_name(Device);
+			}
+			FreePool(out_buf);
+		}
 	}
-	putchar( '\n' ) ;
 
-	hush_next_line() ;
-
-	argv[ 1 ] = UEFI_disk_name( Disk ) ;
-	status = main( 2 , argv , envp ) ;
+	status = main( (int)argc , args , envp ) ;
 
 	if ( status != EFI_SUCCESS ) status = EFI_ABORTED ;
 	return status ;
